bb22ba7820f1928e2094b7cede81e324514177ba
atl043 <atl043@ucsd.edu>
2019-02-28 09:52:04 -0800

    lab8

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,8 +1,8 @@
 // 'use strict';
 
 // Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
-  initializePage();
+$(document).ready(function () {
+	initializePage();
 })
 
 /*
@@ -10,18 +10,55 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Page ready");
- 	// initCamera();
- 	// initMap();
- 	initGestures();
- 	initRSVPForm();
+	initCamera();
+	initMap();
+	initGestures();
+	initRSVPForm();
 }
 
 // init jQuery gestures  
 function initGestures() {
 	// add gestures listener here
+	$(function () {
+		$(".judge-img").bind("taphold", tapholdHandler);
+		function tapholdHandler(event) {
+			var targetIDPrefix = event.target.id;
+			console.log("got prefix: " + targetIDPrefix);
+			$('#' + targetIDPrefix + "-bio").show();
+		}
+	});
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
-  // add your code here
+	// add your code here
+	$('#rsvpForm').submit(function (e) {
+		e.preventDefault();
+		console.log("submitting form ...")
+		var rsvpEmail = $('#rsvpEmail').val();
+		$.post('addRSVP', { rsvpEmail: rsvpEmail }, postCallback);
+	});
+	function postCallback(res) {
+		alert("rsvp form successfully submitted");
+		$('#rsvpEmail').val(''); // clear form
+	}
 }
+
+function initMap() {
+	L.mapquest.key = 'pQAvg8uiMeOzAutnlmrCdvv20eqjzAYG';
+	var map = L.mapquest.map('map', {
+		center: [32.878916, - 117.235909],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.88, -117.235861]).addTo(map);
+}
+
+$(function () {
+	$("div.box").bind("taphold", tapholdHandler);
+	function tapholdHandler(event) {
+		$(event.target).addClass("taphold");
+	}
+});
